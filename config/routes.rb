10bcfb7eb2fcc6112a8a93eccd7d6cf02e50d1e1
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2017-02-24 21:37:29 +0900

    letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'posts' => 'posts#index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   resources :posts do
     collection do
       post :confirm
